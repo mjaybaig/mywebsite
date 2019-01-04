4b56dfee42a49924388469716af404ab1740e6d1
M. Jameel Baig <mjameelbaig@hotmail.com>
2019-01-05 01:04:18 +0500

    Added some sections

--- app.js
+++ app.js
@@ -9,9 +9,11 @@ const port = process.env.PORT || 3000;
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
-app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
-app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
-app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/@fortawesome/fontawesome-free/css')));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/popper.js/dist')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views/index.html'));
---
+++ public/js/script.js
@@ -0,0 +1,35 @@
+
+// class TabHandler{
+//   constructor(){
+//     this.tabLink = [];
+//     this.tabPanels = [];
+//   }
+
+//   populateFields(){
+//     this.tabLink = document.getElementById("mainnav").getElementsByTagName("li");
+//     this.tabPanels = document.getElementById("navcontent").querySelectorAll("div .col-sm-12");
+//   }
+// }
+
+
+// const myTabs = new TabHandler();
+
+
+// window.onload = function(){
+//   // for(var i = 0; i<tabLink.length; i++){
+//   //   console.log(tabLink[i]);
+//   //   tabLink[i].addEventListener("click", selectTab);
+//   // }
+//   // console.log("**********************************************");
+//   myTabs.populateFields()
+
+//   console.log(myTabs.tabLink);
+// }
+
+// // function selectTab(){
+// //   console.log(this);
+// //   console.log(tabPanels);
+// //   // for(var i = 0; i<tabPanels.length; i++){
+// //   //   console.log(tabPanels[i]);
+// //   // }
+// // }
